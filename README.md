6867947d703b793244e3cbf3466772dd1268a3c8
Vasilii Novikov <n1dr+cm3513git@ya.ru>
2019-08-26 17:39:00 +0200

    make the api more obvious, clean up

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## rm -rf  [![Build Status](https://travis-ci.org/vn971/rm_rf.svg?branch=master)](https://travis-ci.org/vn971/rm_rf)  [![crates.io](https://img.shields.io/crates/v/rm_rf.svg)](https://crates.io/crates/rm_rf)
 
-Force-removes a file/directory and all descendants.
+Force-remove a file/directory and all descendants.
 
 In contrast to `std::fs::remove_dir_all`, it will remove
 empty directories that lack read access on Linux,
@@ -10,12 +10,7 @@ and will remove "read-only" files and directories on Windows.
 ## Usage
 
 ```rust
-extern crate rm_rf;
-
-fn main() {
-    // Failure may still happen, in situations identical to where `rm -rf` would fail.
-    rm_rf::force_remove_all("target", true).expect("Failed to remove file/directory");
-}
+rm_rf::force_remove_all("target")?;
 ```
 
 Note: to avoid stack overflow for deeply nested directories, this library uses [stacker](https://crates.io/crates/stacker).
--- src/lib.rs
+++ src/lib.rs
@@ -2,110 +2,137 @@ extern crate stacker;
 
 use std::fs;
 use std::io;
+use std::io::ErrorKind;
 use std::path::Path;
-
-#[cfg(target_os = "windows")]
-fn fix_permissions(path: &Path) -> io::Result<()> {
-	let mut permissions = fs::metadata(&path)?.permissions();
-	permissions.set_readonly(false);
-	fs::set_permissions(&path, permissions)
-}
-
-#[cfg(not(target_os = "windows"))]
-fn fix_permissions(_: &Path) -> io::Result<()> {
-	Ok(())
-}
-
 
 /// Force-removes a file/directory and all descendants.
 ///
 /// In contrast to `std::fs::remove_dir_all`, it will remove
 /// empty directories that lack read access on Linux,
 /// and will remove "read-only" files and directories on Windows.
-///
-/// The current implementation is suboptimal in terms
-/// of performance (e.g. number of syscalls made), but it should work.
-pub fn force_remove_all<P: AsRef<Path>>(path: P, ignore_not_existing: bool) -> io::Result<()> {
-	let path = path.as_ref();
-	if ignore_not_existing && !path.exists() {
-		return Ok(())
-	}
-	fix_permissions(path)?;
-	if fs::remove_file(path).is_ok() {
-		Ok(())
-	} else if fs::remove_dir(path).is_ok() {
-		Ok(())
-	} else {
-		for child in fs::read_dir(&path)? {
-			let child = child?;
-			let path = child.path();
-			stacker::maybe_grow(4 * 1024, 16 * 1024, ||
-				// don't die with stack overflow for deeply nested directories
-				force_remove_all(&path, false),
-			)?;
-		}
-		fs::remove_dir(path)
-	}
+pub fn force_remove_all<P: AsRef<Path>>(path: P) -> io::Result<()> {
+    let path = path.as_ref();
+    match path.metadata() {
+        Ok(_) => force_remove_all_fail_if_not_exist(path),
+        Err(err) => match err.kind() {
+            ErrorKind::NotFound => Ok(()),
+            _ => Err(err),
+        },
+    }
 }
 
+/// same as `force_remove_all` above, but fail if the original destination does not exist
+pub fn force_remove_all_fail_if_not_exist(path: &Path) -> io::Result<()> {
+    fix_permissions(path)?;
+    if path.is_file() {
+        fs::remove_file(path)
+    } else if fs::remove_dir(path).is_ok() {
+        Ok(())
+    } else {
+        for child in fs::read_dir(&path)? {
+            let child = child?;
+            let path = child.path();
+            stacker::maybe_grow(4 * 1024, 16 * 1024, ||
+        // don't die with stack overflow for deeply nested directories
+        force_remove_all_fail_if_not_exist(&path))?;
+        }
+        fs::remove_dir(path)
+    }
+}
+
+#[cfg(target_os = "windows")]
+fn fix_permissions(path: &Path) -> io::Result<()> {
+    let mut permissions = fs::metadata(&path)?.permissions();
+    permissions.set_readonly(false);
+    fs::set_permissions(&path, permissions)
+}
+
+#[cfg(not(target_os = "windows"))]
+fn fix_permissions(_: &Path) -> io::Result<()> {
+    Ok(())
+}
 
 #[cfg(test)]
-#[cfg(not(target_os = "windows"))]  // windows may not have `rm`, `sh` and `chmod`
+#[cfg(not(target_os = "windows"))] // windows may not have `rm`, `sh` and `chmod`
 mod tests {
-	use std::process::Command;
+    use std::process::Command;
 
-	fn rm_rf_success() -> bool {
-		Command::new("rm").arg("-rf").arg("target").output().unwrap().status.success()
-	}
+    #[test]
+    fn behavior_test() {
+        test_eq_behavior("touch target");
+        test_eq_behavior("touch target; chmod 000 target");
+        test_eq_behavior("touch target; chmod 777 target");
 
-	fn rust_remove_success() -> bool {
-		super::force_remove_all("target", true).is_ok()
-	}
+        test_eq_behavior("mkdir target");
+        test_eq_behavior("mkdir target; chmod 000 target");
+        test_eq_behavior("mkdir target; chmod 777 target");
 
-	fn clean() {
-		Command::new("sh").arg("-c").arg("chmod 777 target; rm -rf target").output().unwrap();
-	}
+        test_eq_behavior("mkdir -p target/subdir; chmod 000 target");
+        test_eq_behavior("mkdir -p target/subdir; chmod 777 target");
+        test_eq_behavior("mkdir -p target/subdir; chmod 444 target");
+        test_eq_behavior("mkdir -p target/subdir; chmod 222 target");
+        test_eq_behavior("mkdir -p target/subdir; chmod 111 target");
 
-	fn test_eq_behavior_fn<F>(up: &F, test_name: &str) where F: Fn() -> () {
-		std::env::set_current_dir("target").unwrap();
-		clean();
-		up();
-		let rm_success = rm_rf_success();
-		clean();
-		up();
-		let rust_success = rust_remove_success();
-		clean();
-		eprintln!("Running test {}. `rm -rf` success: {}, `force_remove` success: {}",
-			test_name, rm_success, rust_success);
-		assert_eq!(rm_success, rust_success,
-			"`rm -rf` and `force_remove` behaved differently for test: {}",
-			test_name
-		);
-		std::env::set_current_dir("..").unwrap();
-	}
+        test_eq_behavior("");
+    }
 
-	fn test_eq_behavior(up: &str) {
-		test_eq_behavior_fn(&|| {
-			assert!(Command::new("sh").arg("-c").arg(up).status().unwrap().success());
-		}, up);
-	}
+    fn test_eq_behavior(up: &str) {
+        test_eq_behavior_fn(
+            &|| {
+                assert!(Command::new("sh")
+                    .arg("-c")
+                    .arg(up)
+                    .status()
+                    .unwrap()
+                    .success());
+            },
+            up,
+        );
+    }
 
-	#[test]
-	fn behavior_test() {
-		test_eq_behavior("touch target");
-		test_eq_behavior("touch target; chmod 000 target");
-		test_eq_behavior("touch target; chmod 777 target");
+    fn test_eq_behavior_fn<F>(up: &F, test_name: &str)
+    where
+        F: Fn() -> (),
+    {
+        std::env::set_current_dir("target").unwrap();
+        clean();
+        up();
+        let rm_success = rm_rf_success();
+        clean();
+        up();
+        let rust_success = rust_remove_success();
+        clean();
+        eprintln!(
+            "Running test {}. `rm -rf` success: {}, `force_remove` success: {}",
+            test_name, rm_success, rust_success
+        );
+        assert_eq!(
+            rm_success, rust_success,
+            "`rm -rf` and `force_remove` behaved differently for test: {}",
+            test_name
+        );
+        std::env::set_current_dir("..").unwrap();
+    }
 
-		test_eq_behavior("mkdir target");
-		test_eq_behavior("mkdir target; chmod 000 target");
-		test_eq_behavior("mkdir target; chmod 777 target");
+    fn clean() {
+        Command::new("sh")
+            .arg("-c")
+            .arg("chmod 777 target; rm -rf target")
+            .output()
+            .unwrap();
+    }
 
-		test_eq_behavior("mkdir -p target/subdir; chmod 000 target");
-		test_eq_behavior("mkdir -p target/subdir; chmod 777 target");
-		test_eq_behavior("mkdir -p target/subdir; chmod 444 target");
-		test_eq_behavior("mkdir -p target/subdir; chmod 222 target");
-		test_eq_behavior("mkdir -p target/subdir; chmod 111 target");
+    fn rm_rf_success() -> bool {
+        Command::new("rm")
+            .arg("-rf")
+            .arg("target")
+            .output()
+            .unwrap()
+            .status
+            .success()
+    }
 
-		test_eq_behavior("");
-	}
+    fn rust_remove_success() -> bool {
+        super::force_remove_all("target").is_ok()
+    }
 }
